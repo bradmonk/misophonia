0b9012fa0b774342de5ca8e9ee687d4f4c8e56a8
Bradley Monk <brad.monk@gmail.com>
2015-09-11 01:17:23 -0700

    initial commit

---
+++ FrameOrdering.m
@@ -0,0 +1,26 @@
+function [FrameO] = FrameOrdering(trial_data)
+FrameO = []; % Frame order vec
+FrameO(1:5) = NaN; % For subjects 3 and 4, first five trials are removed
+CSm = [1 2 3 4 5 6 7 8]; % CS- codes
+CSpp = [9 10 11 12 13 14 15 16]; % CS+ paired codes
+CSpu = [17 18 19 20 21 22 23 24]; % CS+ unpaired codes
+acq_trials = length(find(strcmp(trial_data.phase,'Acquisition'))); % Determine number of acquisition trials
+
+for i = 1:acq_trials
+    if trial_data.stim(i,1) == 0 % CS- trials
+        FrameO = [FrameO CSm];
+    elseif trial_data.stim(i,1) == 1 && trial_data.shock(i,1) == 1 % CS+ paired trials
+        FrameO = [FrameO CSpp];
+    else % CS+ unpaired trials
+        FrameO = [FrameO CSpu];
+    end
+end
+
+% Get rid of the first five trials
+for j = 1:5
+    FrameO(1) = [];
+end
+
+% save('FrameO', 'FrameO');
+end
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# misophonia
-misophonia research study code
+# thermal - Thermal Imaging Experiments
+
+<img src="http://camk2.com/pix/Tau2.png" width="300" border="10" />
+
+
+# Online Resources
+* <a href="http://www.flir.com/cores/display/?id=54717" target="_blank">FLIR Tau 640 longwave-infrared (LWIR) 9 mm camera</a>
+* <a href="http://www.flir.com/cores/display/?id=51880" target="_blank">FLIR Tau Software</a>
+* <a href="https://www.elgato.com/en/video/video-capture/support" target="_blank">elgato video capture</a>
+* <a href="https://www.silabs.com/products/mcu/Pages/USBtoUARTBridgeVCPDrivers.aspx" target="_blank">USB to UART Bridge VCP Drivers</a>
+
